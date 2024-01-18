17ee4e3a5bf459c5a47096295c1f76d93368a2f3
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2024-01-18 08:19:35 -0300

    Create Convertir_hex.py

---
+++ Convertir_hex.py
@@ -0,0 +1,17 @@
+def convertir_a_hexadecimal(datos_entrada):
+    try:
+        # Convierte los datos de entrada a hexadecimal
+        salida_hexadecimal = ''.join(format(ord(caracter), '02x') for caracter in datos_entrada)
+        return salida_hexadecimal
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    # Obtén la entrada del usuario
+    entrada_usuario = input("Ingresa los datos a convertir a hexadecimal: ")
+
+    # Convierte la entrada a hexadecimal
+    resultado_hexadecimal = convertir_a_hexadecimal(entrada_usuario)
+
+    # Muestra el resultado
+    print(f"Representación hexadecimal: {resultado_hexadecimal}")
