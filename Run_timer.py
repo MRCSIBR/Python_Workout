48a9d5508276e1aed06b33743298e5b379526846
Marcos D. Ibarra <127572358+MRCSIBR@users.noreply.github.com>
2024-01-18 08:24:05 -0300

    Update Run_timer.py

--- Run_timer.py
+++ Run_timer.py
@@ -1,13 +1,13 @@
 import time
 
 def run_timer():
-    input("Press Enter to start the timer...")
+    input("Presiona Enter para comenzar el timer...")
     start_time = time.time()
-    input("Press Enter to stop the timer...")
+    input("Presiona Enter para detener el timer...")
     end_time = time.time()
 
     elapsed_time = end_time - start_time
-    print(f"\nElapsed time: {elapsed_time:.2f} seconds")
+    print(f"\nTiempo transcurrido: {elapsed_time:.2f} seconds")
 
 if __name__ == "__main__":
     run_timer()
